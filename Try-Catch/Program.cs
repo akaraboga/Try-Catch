49b9b27bc34ab8360ed4d4c7291994d8693c7131
Ahmet Karaboğa <a.blackbull1453@gmail.com>
2024-11-25 15:41:17 +0300

    Add project files.

---
+++ Try-Catch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+            }catch (Exception)
+            {
+                Console.WriteLine("Gecersiz karakter girdiniz");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
